074d04c1ed5542b5fef9a8defbfc91ed52c883af
Robert Horvers <robert.horvers@cz.nl>
2025-01-14 15:26:13 +0100

    add basepath for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export"
+  output: "export",
+  basePath: "/poc-dependency-pipeline"
 };
 
 export default nextConfig;
