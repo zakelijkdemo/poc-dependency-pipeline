9da159c068e0c3fe8faffbbda4e451d197166afb
Remko van Laarhoven <remko.van.laarhoven@cz.nl>
2025-01-15 15:50:23 +0100

    ci: update output location in Azure Static Web Apps workflow and change Next.js output configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export",
+  output: "standalone",
+  distDir: 'dist',	
   basePath: "/poc-dependency-pipeline"
 };
 
