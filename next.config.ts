8f849bc053597d716139b77d1264866083003331
Remko van Laarhoven <remko.van.laarhoven@cz.nl>
2025-01-15 15:24:12 +0100

    chore: update .gitignore and configure Next.js to use 'dist' directory for output

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  distDir: 'dist',
   basePath: "/poc-dependency-pipeline"
 };
 
