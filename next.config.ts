6ecf3956a647a4a4bb33fdbf2df79865512d87be
Skip van Laarhoven <remko.van.laarhoven@gmail.com>
2025-01-16 08:54:45 +0000

    update for static web app

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,28 @@ const nextConfig: NextConfig = {
   // output: "export",
   output: "standalone",
   distDir: 'dist',	
-  basePath: "/poc-dependency-pipeline"
+  // basePath: "/poc-dependency-pipeline"
+
+  async redirects() {
+    return [
+      {
+        source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+        destination: '<YOUR REDIRECT RULE>', 
+        permanent: false,
+      },
+    ]
+},
+async rewrites() {
+  return {
+      beforeFiles: [
+          {
+              source: '/((?!.swa).*)<YOUR MATCHING RULE>',
+              destination: '<YOUR REWRITE RULE>', 
+          }
+      ]
+  }
+}
 };
+
 
 export default nextConfig;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export const config = {
     matcher: [
-      '/((?!_next/static|_next/image|.swa/health|favicon.ico).*)',
+      '/((?!.swa).*)',
     ],
   };
